5179cc5ee6f04998ead39da6544cc19d4bb4490c
ruben13pulido-prog <ruben13pulido@gmail.com>
2026-03-27 22:03:48 -0700

    Fix syntax error in GET route definition

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) >= {
+app.get('/', (req, res) => {
   res.send('Bot is alive!');
 });
 
