5b2cb7faea4cfa9a93c0f9a1fd5e9fcfb051c8ab
ruben13pulido-prog <ruben13pulido@gmail.com>
2026-04-03 21:04:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Bot is alive!');
+app.listen(3000, () => {
+  console.log("Web server is running");
 });
 
 const {
